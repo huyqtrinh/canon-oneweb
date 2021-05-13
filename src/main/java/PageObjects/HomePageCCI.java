dbaedd753079685366520fe514f9461e5c4aafc8
huyqtrinh.sp <huy.trinh@hitachivantara.com>
2021-05-13 17:01:42 +0700

    Complete tc login/logout

--- src/main/java/PageObjects/HomePageCCI.java
+++ src/main/java/PageObjects/HomePageCCI.java
@@ -10,6 +10,8 @@ public class HomePageCCI {
     By btn_Cameras = By.xpath("//a[@href='https://mcstaging-estore.canon.ca/en_ca/cameras']");
     By btn_Lenses = By.xpath("//a[@href='https://mcstaging-estore.canon.ca/en_ca/lenses']");
     By btn_ClosePopupPromo = By.xpath("//button[@data-role='closeBtn']");
+    By btn_Logout = By.id("mcLogout");
+
     //Click user icon
     public HomePageCCI(WebDriver driver) {
         this.driver = driver;
@@ -37,5 +39,9 @@ public class HomePageCCI {
     public void clickOnLensesBtn() {
         WebElement btnLenses = driver.findElement(btn_Lenses);
         btnLenses.click();
+    }
+    public void clickOnButtonLogout() {
+        WebElement btnLogout = driver.findElement(btn_Logout);
+        btnLogout.click();
     }
 }
--- src/test/java/TestLogin.java
+++ src/test/java/TestLogin.java
@@ -6,9 +6,10 @@ import PageObjects.LoginPage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
 public class TestLogin {
     String driverPath = "src/main/resources/WebDrivers/Chrome/chromedriver.exe";
     WebDriver driver;
@@ -16,28 +17,33 @@ public class TestLogin {
     LoginPage objLoginPage;
 
     @BeforeTest
-    public void setup(){
+    public void setup() {
         System.setProperty("webdriver.chrome.driver", driverPath);
         driver = new ChromeDriver();
-        //Step 1: Navigate to the CCI(Canada) application (https://mcstaging-estore.canon.ca/en_ca/)
-        driver.get("https://mcstaging-estore.canon.ca/en_ca/");
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-
     }
 
-    @Test(priority=0)
-    public void test_Login_Logout(){
-        //Create HomePage object
+    @Test()
+    public void test_Login_Logout() {
         objHomePage = new HomePageCCI(driver);
         objLoginPage = new LoginPage(driver);
         try {
+            //Step 1: Navigate to the CCI(Canada) application (https://mcstaging-estore.canon.ca/en_ca/)
+            driver.manage().window().maximize();
+            driver.get("https://mcstaging-estore.canon.ca/en_ca/");
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+            System.out.println("Navigate to page : " + driver.getTitle());
+
+            //Close popup promo
             objHomePage.closePopupPromo();
             Thread.sleep(2000);
-            String strTitle = driver.getTitle();
+            String strHomeTitle = driver.getTitle();
+
             //Step 2: Log into the application with above credentials clicking on user icon to the top right corner of the page
             objHomePage.clickOnUserIcon();
             Thread.sleep(2000);
+            Boolean display = objHomePage.checkBtnLoginDisplayed();
+            Assert.assertEquals(display, Boolean.TRUE);
+            System.out.println("Button login displayed.");
             objHomePage.clickOnButtonLogin();
             Thread.sleep(5000);
             objLoginPage.loginToCCI();
@@ -45,7 +51,9 @@ public class TestLogin {
 
             //Step 3: On successful login, user will be navigated to the same page where the user logged in from
             String strCurTitle = driver.getTitle();
-            Assert.assertEquals(strTitle,strCurTitle);
+            Assert.assertEquals(strCurTitle,strHomeTitle );
+            System.out.println("Login successful. User navigated to the same page where the user logged in from.");
+
 
             //Step 4: Browse the site (navigate to one or two pages)
             objHomePage.clickOnCamerasBtn();
@@ -54,9 +62,30 @@ public class TestLogin {
             objHomePage.clickOnLensesBtn();
             Thread.sleep(5000);
             System.out.println("Navigate to page : " + driver.getTitle());
+
+            //Step 5: Click on the logout link and the user must log out successfully and must be redirected to home page.
+            objHomePage.clickOnUserIcon();
+            Thread.sleep(2000);
+            objHomePage.clickOnButtonLogout();
+            Thread.sleep(5000);
+            strCurTitle = driver.getTitle();
+            Assert.assertEquals(strCurTitle,strHomeTitle );
+            System.out.println("Logout successful. User redirected to home page.");
+
+            //Step 6: Reconfirm that the user is logged out by clicking on the user icon on the top right corner of the page
+            objHomePage.clickOnUserIcon();
+            Thread.sleep(2000);
+            display = objHomePage.checkBtnLoginDisplayed();
+            Assert.assertEquals(display, Boolean.TRUE);
+            System.out.println("Reconfirm that the user is logged out. Button Login is displayed.");
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @AfterTest
+    public void closeDriver() {
         driver.quit();
     }
 
