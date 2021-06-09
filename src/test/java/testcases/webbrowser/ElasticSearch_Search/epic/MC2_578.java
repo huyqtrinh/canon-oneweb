9af8b3d2b41696997361f705432306ed7c10b746
Sinh Nhat Nguyen <sinh.nguyen1@hitachivantara.com>
2021-06-09 10:40:01 +0700

    [SN] update QA homepage UI and MyAction module for searching

--- src/test/java/testcases/webbrowser/ElasticSearch_Search/epic/MC2_578.java
+++ src/test/java/testcases/webbrowser/ElasticSearch_Search/epic/MC2_578.java
@@ -10,9 +10,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.Reporter;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
 
 public class MC2_578 {
     WebDriver driver;
@@ -28,22 +28,27 @@ public class MC2_578 {
     String Ex_NoSearchResult_3 = "Browse our popular categories or products";
     String Ex_NoSearchResult_4 = "Contact Support";
 
-    @BeforeTest
+    @BeforeMethod
     public void StartBrowser() {
-        driver = LaunchBrowser.getDriver(Constants.Browser, Constants.MagentoQA_Url);
+        driver = LaunchBrowser.getDriver(Constants.Browser);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
 
     @Test()
     public void HIT_MC2_578_TC_001_Verify_Search_Results_screen_when_there_are_no_search_results() throws InterruptedException {
         Reporter.log("Start HIT_MC2_578_TC_001 on browser " + Constants.Browser);
 
-        // [SN] Add handling for promo popup
+        Reporter.log("Step 1: Launch the Home page for CCI");
+        driver.get(Constants.MagentoQA_Url);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         MyActions.HandlingPromoPopup(driver);
+        Thread.sleep(12000);
+        Reporter.log("Step 2: Enter the text \"@#$!%\"  in search text box and press Enter");
         Search.SearchString(driver, var_invalidSearchString);
-        Thread.sleep(2000);
+        Thread.sleep(12000);
 
         try {
-            Reporter.log("Get and verify Search Result");
+            Reporter.log("Step 3: Get and verify Search Result");
 
             String VP_NoSearchResult_1 = QAHomePage.lb_NoSearchResult_Line1(driver).getText();
             String VP_NoSearchResult_2 = QAHomePage.lb_NoSearchResult_Line2(driver).getText();
@@ -61,20 +66,23 @@ public class MC2_578 {
             Reporter.log(e.toString());
             Reporter.log("The Search Result does NOT display correctly as expected");
         }
-
     }
 
     @Test()
     public void HIT_MC2_578_TC_002_Verify_Search_Results_screen_when_there_are_no_results_for_users_search_terms_and_synonyms_for_the_search_terms_are_set_up() throws InterruptedException {
         Reporter.log("Start HIT_MC2_578_TC_002 on browser " + Constants.Browser);
 
-        // [SN] Add handling for promo popup
+        Reporter.log("Step 1: Launch the Home page for CCI");
+        driver.get(Constants.MagentoQA_Url);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         MyActions.HandlingPromoPopup(driver);
+        Thread.sleep(12000);
+        Reporter.log("Step 2: Enter the search string in search text box and press Enter");
         Search.SearchString(driver, var_validSearchString);
-        Thread.sleep(2000);
+        Thread.sleep(12000);
 
         try {
-            Reporter.log("Get and verify Search Result");
+            Reporter.log("Step 3: Verify the products in each results");
 
             Assert.assertTrue(MyActions.checkDisplayed(QAHomePage.lb_SearchResult(driver)));
 
@@ -98,13 +106,18 @@ public class MC2_578 {
     @Test()
     public void HIT_MC2_578_TC_003_Verify_CMS_block_when_Search_Results_screen_when_there_are_no_results() throws InterruptedException {
         Reporter.log("Start HIT_MC2_578_TC_003 on browser " + Constants.Browser);
-        // [SN] Add handling for promo popup
+
+        Reporter.log("Step 1: Launch the Home page for CCI");
+        driver.get(Constants.MagentoQA_Url);
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
         MyActions.HandlingPromoPopup(driver);
+        Thread.sleep(12000);
+        Reporter.log("Step 2: Enter the synonym search string in search text box and press Enter");
         Search.SearchString(driver, var_incorrectSearchString);
-        Thread.sleep(2000);
+        Thread.sleep(12000);
 
         try {
-            Reporter.log("Get and verify Search Result");
+            Reporter.log("Step 3: Get and verify Search Result");
 
             Assert.assertTrue(MyActions.checkDisplayed(QAHomePage.lb_SearchResult(driver)));
 
@@ -112,15 +125,9 @@ public class MC2_578 {
             int VP_NumofSearchResult = Integer.parseInt(QAHomePage.lb_SearchResultNumber(driver).getText());
 
             for (int i = 1; i <= VP_NumofSearchResult; i++) {
-                // ----------- //*[@id="1"]/div/div/strong/a
                 String CP_CheckItemName = "//*[@id=\"" + i + "\"]/div/div/strong/a";
-                //System.out.println("CP_CheckItemName_" + i + " >>> " + CP_CheckItemName);
 
                 String VP_ItemName = driver.findElement(By.xpath(CP_CheckItemName)).getText().toLowerCase();
-//                System.out.println("-----------------------------------------------");
-//                System.out.println("VP_ItemName >>> " + VP_ItemName);
-//                System.out.println("VP_ItemName.contains(var_incorrectSearchString.toLowerCase() >>> " + VP_ItemName.contains(var_incorrectSearchString.toLowerCase()));
-//                System.out.println("VP_ItemName.contains(var_synonymSearchString.toLowerCase()) >>> " + VP_ItemName.contains(var_synonymSearchString.toLowerCase()));
                 Assert.assertTrue(VP_ItemName.contains(var_incorrectSearchString.toLowerCase()) || VP_ItemName.contains(var_synonymSearchString.toLowerCase()));
             }
             Reporter.log("The Search Result displays correctly as expected");
@@ -129,8 +136,9 @@ public class MC2_578 {
             Reporter.log(e.toString());
         }
     }
-    @AfterTest
+    @AfterMethod
     public void CloseBrowser() {
+        driver.manage().deleteAllCookies();
         driver.quit();
     }
 }
