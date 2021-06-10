436a30b1c4b2b0dc6ce0a16abf8dc57b51d581d2
Sinh Nhat Nguyen <sinh.nguyen1@hitachivantara.com>
2021-06-10 17:00:05 +0700

    [SN] Add test file for ticket 4088

---
+++ src/test/java/testcases/webbrowser/MyOrders/epic/MC2_4088.java
@@ -0,0 +1,11 @@
+package testcases.webbrowser.MyOrders.epic;
+
+import org.testng.annotations.Test;
+
+public class MC2_4088 {
+
+    @Test()
+    public void HIT_MC2_578_TC_001_Verify_Search_Results_screen_when_there_are_no_search_results() throws InterruptedException {
+
+    }
+}
